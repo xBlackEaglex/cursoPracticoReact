06b5423ea65bf75ffb6ddd50fcd66598214dd2f5
xBlackEaglex <roberto.smusic.1@gmail.com>
2022-12-14 23:00:09 -0600

    se integro el header

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => {
-    return ( 
-        <h1>Hola mundo que rollo!</h1>
-    );
-}
-
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/',
     },
     mode: 'development',
     resolve: {
@@ -27,15 +29,32 @@ module.exports = {
                         loader: 'html-loader',
                     }
                 ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin(
-            {
+        new HtmlWebpackPlugin({
                 template: './public/index.html',
                 filename: 'index.html',
-            }
-        )
-    ]
+            }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3005,
+        historyApiFallback: true,
+    }
 }

54a8c23a5a70859a3edeeee1c9353e6da25c27b1
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-01-24 20:40:47 -0600

    se agregaron hooks

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ProductInfo from '../components/ProductInfo';
-import '../styles/ProductDetail.scss';
+import ProductInfo from '@components/ProductInfo';
+import '@styles/ProductDetail.scss';
+
+import productDetail from '@icons/icon_close.png';
 
 const ProductDetail = () => {
 	return (
 		<aside className="ProductDetail">
 			<div className="ProductDetail-close">
-				<img src="./icons/icon_close.png" alt="close" />
+				<img src={productDetail} alt="close" />
 			</div>
 			<ProductInfo />
 		</aside>

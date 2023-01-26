a737083d05835d08cce7cc22e9b72506394f172a
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-01-25 22:24:38 -0600

    se agrego el area de carrito

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import OrderItem from '@components/OrderItem';
+import AppContext from '@context/AppContext';
 import '@styles/MyOrder.scss';
 
 import flechita from '@icons/flechita.svg';
 
 const MyOrder = () => {
+	const {state} = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
+
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -12,12 +21,14 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(product => (
+					<OrderItem product = {product} key={`orderItem-${product.id}`} />
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
